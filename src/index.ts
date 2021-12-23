619d28c472b3fb5e4440f24da29f70e2509a5ea6
Tearat <raiiliiat@yandex.ru>
2021-12-23 16:52:30 +0500

    init

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata"
+import { createConnection, getRepository } from "typeorm"
+import { User } from "./entity/User"
+import express from "express"
+
+createConnection().then(async (connection) => {
+  const app = express()
+
+  app.get("/", async (req, res) => {
+    const users = await User.all()
+    return res.send(users)
+  })
+
+  app.get("/:id", async (req, res) => {
+    const id: number = parseInt(req.params.id)
+    const user = await User.find({ id })
+    return res.send(user)
+  })
+
+  app.get("/add", async (req, res) => {
+    const { name = "Anonymous", age = 10 } = req.query as any
+    User.create({
+      name,
+      age,
+    }).save()
+    res.send({ status: `Created` })
+  })
+
+  const port = process.env.PORT || 8000
+  app
+    .listen(port, () => console.log(`>>> Server starts (port ${port})`))
+    .on("error", (error) => console.warn(error))
+})
