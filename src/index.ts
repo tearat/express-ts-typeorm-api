c68c457e295cbbd0ef48558c76e8ebb161f194d9
Tearat <raiiliiat@yandex.ru>
2022-01-02 22:23:11 +0500

    Fix routing

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,23 @@ import express from "express"
 
 createConnection().then(async (connection) => {
   const app = express()
-
   app.get("/", async (req, res) => {
     const users = await User.all()
     return res.send(users)
   })
-
-  app.get("/:id", async (req, res) => {
+  app.get("/:id(\\d+)/", async (req, res) => {
     const id: number = parseInt(req.params.id)
-    const user = await User.find({ id })
+    const user = await User.findOne({ id })
     return res.send(user)
   })
-
   app.get("/add", async (req, res) => {
     const { name = "Anonymous", age = 10 } = req.query as any
-    User.create({
+    await User.create({
       name,
       age,
     }).save()
-    res.send({ status: `Created` })
+    return res.send({ status: `Created` })
   })
-
   const port = process.env.PORT || 8000
   app
     .listen(port, () => console.log(`>>> Server starts (port ${port})`))
